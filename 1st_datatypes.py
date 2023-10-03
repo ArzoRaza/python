73ebb7605052fd46c70037a5afdf75b3e474475b
ArzoRaza <ccoding76@gmail.com>
2023-10-03 18:39:16 +0530

    add file of datatypes

---
+++ 1st_datatypes.py
@@ -0,0 +1,35 @@
+# In this file we see all data type is python language, let's start from scratch 
+
+
+#Basic types(Number, float, String, Boolean, Complex)
+# Number 
+print(type(79))
+
+# float
+print(type(34.343))
+
+# string =>  we declare string in tree types ('') - ("") - (""" """)
+print(type('Japan'))
+print(type("Japan"))
+print(type("""Japan"""))
+
+# Boolean 
+print(type(True))
+
+# Complex 
+print(type(3+5j))
+
+
+
+# Countainer types (list, tuple, set, dectionary)
+# List
+print(type([1, 3, 5, 7, 9]))
+
+# tuple 
+print(type((1, 3, 5, 5, 6, 7)))
+
+# set 
+print(type({1, 3, 5, 6, 7, 8}))
+
+# dectionary
+print(type({"Name": "Md akbar ali", "age": 20, "address": "Bihar"}))
