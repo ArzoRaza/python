8d22fa89af4482d6f5462c021857c9849a0c213b
ArzoRaza <ccoding76@gmail.com>
2023-12-05 19:55:10 +0530

    this file have details of literals

---
+++ literals.py
@@ -0,0 +1,43 @@
+# let's talk about literals 
+
+# ****************numeric literals****************** 
+a = 0b1010 #Binary literals 
+b = 100  # decimal literal 
+c = 0o310 # octal litral 
+d = 0x12c # Hecadecimal literal 
+
+# float literal 
+float_1 = 10.5
+float_2 = 1.5e2
+float_3 = 1.5e-3
+
+# ****************Conplex Literal *****************
+x = 3.14j
+
+# print(a, b, c, d)
+
+
+
+# ****************String literals *****************
+
+string = 'This is python program'
+strings = "also it is"
+char = "C"
+multiline_str = """This is a multiline code more than one line code."""
+
+# print(string)
+# print(strings)
+# print(char)
+# print(multiline_str)
+
+
+# ***************** Boolean Literals *******************
+a = True + 4 
+b = False + 10 
+# print("a =", a)
+# print("b =", b)
+
+
+# *****************  Special Literal *******************
+a = None  # we use none for Variable declar 
+# print(a)
