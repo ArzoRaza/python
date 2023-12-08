8d37b62a17c4e13feef283fecaba3f9ad8043672
ArzoRaza <ccoding76@gmail.com>
2023-12-08 12:37:47 +0530

    it file about For Loop

---
+++ 07_for_loop.py
@@ -0,0 +1,18 @@
+# let's learn about For Loop 
+
+# IN for loop we descus about how we declare for loop and what the difference bitween for loop and while loop 
+
+"""
+while loop we use when we don't know about how many time process going on, there we need while loop.
+but 
+for loop we use then we know this process going of 10 times then we use for loop.
+"""
+
+for i in range(0, 10):
+    print(i)
+
+
+# range() can take 3 arguments 1st one is start and 2nd one is stop and 3rd is steps let's see examples 
+
+for i in range(1, 20, 3):
+    print(i) # start printing from 1 to 20 but gap bitween 3 because of we declare 3 on steps part
