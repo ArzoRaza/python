a88ad7d3d18beb98e041f7cfeae7bc5c7ad73f2c
ArzoRaza <ccoding76@gmail.com>
2023-12-08 10:53:16 +0530

    In this file see about loop

---
+++ 06_loop.py
@@ -0,0 +1,60 @@
+# In this file we see about Loop, What is the loop and what is the use of loop, some real example of loop. 
+
+# How many types of loop:- generally 3 types of loop have in manly language But not same here in python, python have only 2 loop that call while and for loop.
+ 
+'''
+while 
+for 
+'''
+
+# ***************************************  while ************************************************
+
+"""number = int(input("Enter your number :")) # 20 
+i = 1
+while i <= 10:
+    print(number * i)
+  #  i += 1"""
+
+'''
+Expected Output: 
+20
+40
+60
+80
+100
+120
+140
+160
+180
+200
+'''
+
+'''num = int(input("Enter your number :"))
+i = 1
+while i <= 12:
+    print(num * i)
+    i+=2'''
+
+# let's see table Example 
+
+number = int(input("Enter your number :"))  # 15
+k = 1
+while k < 11:
+    print(number, "*", k, "=", number * k)
+    k += 1
+
+'''
+Expected Output: 
+15 * 1 = 15
+15 * 2 = 30
+15 * 3 = 45
+15 * 4 = 60
+15 * 5 = 75
+15 * 6 = 90
+15 * 7 = 105
+15 * 8 = 120
+15 * 9 = 135
+15 * 10 = 150
+'''
+
+
