8da9063924a22309ad1c73810e9669f5ed05946b
ArzoRaza <ccoding76@gmail.com>
2023-12-09 23:59:38 +0530

    Built in function

---
+++ 10_built_in_fun.py
@@ -0,0 +1,45 @@
+# in this file we explore BUILT IN FUNCTION 
+
+# built in function nothing but it is pre created function in python language and we use it, like this:- print(), input(), pow(), etc. 
+
+# let's see some popular Example of built in function
+
+# 1 
+# print("Hello")
+#Exp OUTPUT: Hello
+
+# 2 
+# input("Enter your name :")
+#Exp OUTPUT: Enter your name : ...........
+
+# 3 
+# print(type(4))
+#Exp OUTPUT: int
+
+# 4 
+# print(int(True ))  
+# Exp OUTPUT : 1
+
+# 5
+# print(abs(-33))
+# Exp OUTPUT: 33
+
+#6 
+# print(pow(2, 3))
+# Exp OUTPUT: 8
+
+# 7 
+print(min(1, 2, 3, 4, 5))
+# Exp OUTPUT: 1
+
+# 8 
+print(max(1, 3, 79, 34, 101))
+# Exp OUTPUT: 101
+
+# 9 
+c = 22/7
+print(round(c, 3))
+# Exp OUTPUT: 3.143
+
+# 10 
+help(print)
