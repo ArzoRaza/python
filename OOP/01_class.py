8c0324f6814180f40e0faffbe79285a88a433c08
ArzoRaza <ccoding76@gmail.com>
2023-12-15 00:51:37 +0530

    This file about OOPs

---
+++ OOP/01_class.py
@@ -0,0 +1,14 @@
+'''
+In this file we discuse about OOP (Object Oriented Programming).
+Types of OOP:-  
+1 - Class 
+2 - Polymorphism 
+3 - Encapsulation 
+4 - Inheritance 
+5 - Abstraction 
+6 - Object 
+'''
+ # Let's first talk about Class 
+
+ #  ***************************************  Class  **************************************
+ 
