a29c29d6a0dcdfd46aa883fdd6bd427167f130bc
ArzoRaza <ccoding76@gmail.com>
2024-07-07 00:18:29 +0530

    Indian population calculation

---
+++ 02_questions/01_populationCalculation.py
@@ -0,0 +1,8 @@
+# population calculate of last 10 year. current total population is 10,000.
+
+currnet_population = 1500000000
+
+for i in range (2024, 2010, -1):
+    print( "indian population per year : ", i, int(currnet_population))
+
+    currnet_population = currnet_population - 0.1 * currnet_population
