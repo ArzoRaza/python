f550116049fadbaa221c74c36f585df4aaa44461
ArzoRaza <ccoding76@gmail.com>
2024-07-07 00:51:58 +0530

    Question folder are added

---
+++ 02_allQuestions/01_session_question/01_question.py
@@ -0,0 +1,11 @@
+# Print the given strings as per stated format.
+
+# "You" "will" "only" "fail" "when" "you" "stop"
+
+wordStore = ["You", "will", "only", "fail", "when", "you", "stop"]
+
+newStore = "-".join(wordStore)
+
+print(newStore)
+
+# Expected OUTPUT: You-will-only-fail-when-you-stop
